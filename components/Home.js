7ac67381f3464b5696f58ec5cd8dc5cb4da5ec07
Lotta Laine <lotta-laine@hotmail.com>
2023-03-13 14:34:37 +0200

    Add styles and custom fonts

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { useCallback } from "react";
+import { Text, View } from "react-native";
+import {
+  useFonts,
+  Raleway_400Regular,
+  Raleway_700Bold,
+} from "@expo-google-fonts/raleway";
+import * as SplashScreen from "expo-splash-screen";
+import styles from "./Styles";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Home() {
+  let [fontsLoaded] = useFonts({
+    Raleway_400Regular,
+    Raleway_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    } else {
+      return null;
+    }
+  });
+
   return (
-    <View style={{ justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}>Welcome to my app</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.h1}>Welcome to my app</Text>
     </View>
   );
 }
---
+++ components/Styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Raleway_400Regular",
+    fontSize: 15,
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: "center",
+    fontFamily: "Raleway_400Regular",
+  },
+  h1: {
+    fontSize: 20,
+    fontWeight: "500",
+    fontFamily: "Raleway_700Bold",
+  },
+  listContainer: {
+    padding: 10,
+  },
+});
