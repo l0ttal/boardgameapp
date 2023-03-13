29792232265ed025df5286943ae92818c822988c
Lotta Laine <lotta-laine@hotmail.com>
2023-03-13 13:34:07 +0200

    Create Home page

---
+++ components/Home.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>Welcome to my app</Text>
+    </View>
+  );
+}
